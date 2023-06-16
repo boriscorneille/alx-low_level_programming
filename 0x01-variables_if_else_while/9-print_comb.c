8e2f0dc7d8946f36e9682c33a72835874cc334fb
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-16 10:58:49 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,17 @@
  */
 int main(void)
 {
-	char n;
+	int n;
 
 	for (n= '0' ; n <= '9' ; n++)
+	{
 		putchar(n);
 	if (n != '9')
+	{
 		putchar(',');
+	        putchar(' ');
+	}
+	}
 	putchar('\n');
 	return (0);
 }
