d04a2d67d8b134f2aeea0daed9ae82034d4c5422
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-18 11:01:16 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,18 @@ int main(void)
 {
 	int i,l,n;
 
-	for ( i = '0'; i<= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for ( l = '0'; l<= '9'; l++)
+		for (l = '0'; l <= '9'; l++)
 		{
-			for ( n = '0'; n<= '9'; n++)
+			for ( n = '0'; n <= '9'; n++)
 			{
-				if ( i<l && l<n)
+				if (i < l && l < n)
 				{
 					putchar(i);
 					putchar(l);
 					putchar(n);
-					if ( i != '7')
+					if (i != '7')
 					{
 					putchar(',');
 					putchar(' ');
