c0ca2cb69b3dc1aa4f21c1c92e5b7146645da38e
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-18 10:37:47 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int i,l,n;
+
+	for ( i = '0'; i<= '9'; i++)
+	{
+		for ( l = '0'; l<= '9'; l++)
+		{
+			for ( n = '0'; n<= '9'; n++)
+			{
+				if ( i<l && l<n)
+				{
+					putchar(i);
+					putchar(l);
+					putchar(n);
+					if ((i+l+n) != (7+8+9))
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
