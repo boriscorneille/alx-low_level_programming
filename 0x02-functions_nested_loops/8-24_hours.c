3b1a3f4d9f631d2a4c986a7f26f7aa27da3f6237
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-20 11:25:42 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,15 @@ void jack_bauer(void)
 {
 	int M, H;
 
-	for (M = 0 ; M < 60 ; M++)
+	for (H = 0 ; H < 24 ; H++)
 	{
-		for (H = 0 ; H < 24 ; H++)
+		for (M = 0 ; M < 60 ; M++)
 		{
-			_putchar((M / 10) + '0');
-			_putchar((M % 10) + '0');
-			_putchar(':');
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
 			_putchar('\n');
 		}
 	}
