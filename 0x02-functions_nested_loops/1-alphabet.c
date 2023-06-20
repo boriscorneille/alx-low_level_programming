4059eaecb1a46520512fe32967573fbf23b71a5a
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-20 03:44:46 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * main - print prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+		_putchar(n);
+	_putchar('\n');
+}
