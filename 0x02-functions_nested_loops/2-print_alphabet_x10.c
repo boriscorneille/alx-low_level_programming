094dbdb25cced5c44f207652a6fb85398eae044a
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-20 15:39:32 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
-	char n = 0;
-	
-	while (n <= 9)
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
 	{
-		for (c = 'a' ; c <= 'z' ; c++)
-		{
-			_putchar(c);
-		}
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
-		n++;
 	}
 }
