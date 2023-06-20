ab118108eadac6871b189bfebcaa52884930f397
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-20 15:20:27 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,15 +8,13 @@
  */
 int main()
 {
-	int a;
+	int a, sum= 0;
 
-	if (a < 1024)
+	for (a = 0 ; a < 1024 ; a++)
 	{
-		if ((a / 3) || (a / 5))
-		{
-			printf("%d\n", a);
-			a++;
-		}
+		if ((a / 3) == 0 || (a / 5) == 0)
+			sum = sum + a;
 	}
+	printf("%d\n", sum);
 	return (0);
 }
