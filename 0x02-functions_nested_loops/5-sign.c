7a3ee31e94bfa722b4b3245b0156e929726825d2
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-20 08:13:31 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * print_sign - Check main
+ * @n: An input number
+ * 
+ * Return: 1 if number is positive, o if number is 0, or
+ * -1 if number is negative
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		_putchar('-');
+	return (-1);
+}
