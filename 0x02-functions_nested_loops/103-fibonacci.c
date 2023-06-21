2ae1ed9ecc49341bc9fb91e85d731eaaae366343
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-21 07:03:58 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,19 +9,18 @@
  */
 int main(void)
 {
-	unsigned long fb1 = 0, fb2 = 1, fibsum;
-	float tot_sun;
-	
+	unsigned long fib1 = 0, fib2 = 1, fibsum;
+	float tot_sum;
+
 	while (1)
 	{
-		fibsum = fb1 + fb2;
 		if (fibsum > 4000000)
 			break;
 		if ((fibsum % 2) == 0)
-			tot_sun += fibsum;
-		fb1 = fb2;
-		fb2 = fibsum;
+			tot_sum += fibsum;
+		fib1 = fib2;
+		fib2 = fibsum;
 	}
-	printf("%.0f\n", tot_sun);
+	printf("%.0f", tot_sum);
 	return (0);
 }
