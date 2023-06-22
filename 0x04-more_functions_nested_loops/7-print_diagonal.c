c872ba6fd99a1a7fdc72e53b3fcd62955e444f49
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-22 08:17:13 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,19 +6,19 @@
  */
 void print_diagonal(int n)
 {
-	int leng;
-	int space;
+	int len, space;
 
 	if (n > 0)
 	{
-		for (leng = 0 ; leng < n ; leng++)
+		for (len = 0; len < n; len++)
 		{
-			for (space = 0 ; space < leng ; space++)
-			_putchar(' ');
-		_putchar('\\');
-		if (leng == n-1)
-			continue;
-		_putchar('\n');
+			for (space = 0; space < len; space++)
+				_putchar(' ');
+			_putchar('\\');
+			if (len == n - 1)
+				continue;
+
+			_putchar('\n');
 		}
 	}
 
