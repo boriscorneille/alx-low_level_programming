e37cffce1df5524d297bfecccbe24219ba7f1903
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-22 07:21:40 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,13 +6,12 @@
  */
 void print_line(int n)
 {
+	int a;
 
 	if (n > 0)
 	{
-		_putchar('_');
+		for (a = 0 ; a < n ; a++)
+			_putchar('_');
 	}
-	else
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
