f2a630cc0b7f81e86378aec8ce747938ca027795
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-22 09:22:01 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,26 @@ int main(void)
 {
 	int a;
 
-	for (a = 1 ; a <= 100 ; a++)
+	for (a = 1; a <= 100; a++)
 	{
-		if ((a % 3 ) == 0 && (a % 5) == 0)
+		if ((a % 3) == 0 && (a % 5) == 0)
 			printf("FizzBuzz");
+
 		else if ((a % 3) == 0)
 			printf("Fizz");
+
 		else if ((a % 5) == 0)
 			printf("Buzz");
+
 		else
 			printf("%d", a);
+
 		if (a == 100)
 			continue;
 		printf(" ");
 	}
+
 	printf("\n");
+
 	return (0);
 }
