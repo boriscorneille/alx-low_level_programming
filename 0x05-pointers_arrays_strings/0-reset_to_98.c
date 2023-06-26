f672892687f542a70c3bea9ac6fe14a9985e1cb7
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-26 01:45:25 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,11 @@
 
 
 /**
- * void reset_to_98(int *n) - updates the value it points to to 98
+ * void reset_to_98 - updates the value it points to to 98
  * @n: pointer to be updated
  *
  */
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
