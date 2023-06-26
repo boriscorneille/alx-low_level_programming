34017a3bfd7ec901530e8920fe7e7a5c0dbed8c4
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-26 13:56:25 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,13 @@
  */
 void puts2(char *str)
 {
-	int i = 0, len = 0;
+	int index = 0, len = 0;
 
-	while (str[i++])
+	while (str[index++])
 		len++;
 
-	for (i = 0; i < len; i += 2)
-		putchar(str[i]);
+	for (index = 0; index < len; index += 2)
+		putchar(str[index]);
 
 	putchar('\n');
 }
