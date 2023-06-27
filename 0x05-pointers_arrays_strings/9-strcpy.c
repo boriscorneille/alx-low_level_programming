aabf8697420e0bc1f44a1cb466995aa80239ae8d
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-27 00:30:24 -0700

    9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,16 @@
  */
 char *_strcpy(char *dest,  char *src)
 {
-	int index = 0;
+	int i, len = 0;
 
-	while (src[index])
+	while (src[len] !='\0')
 	{
-		dest[index] = src[index];
-		index++;
+		len++;
 	}
-
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i]= '\0';
 	return (dest);
 }
