6c2e1715dd9a45e1ecd7a085d7deca123d740b83
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-29 08:48:57 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0 ; src[i] != '\0' && i < n ; i++)
-		dest[i] = src [i];
-	for (; i > n ; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; n > i; i++)
 		dest[i] = '\0';
+
 	return (dest);
 }
