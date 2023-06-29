606348d5275f44ee574613be02699f4419aaebf4
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-29 06:04:43 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * _strcmp - Compare two strings
  * @s1: string
@@ -12,15 +13,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
 	{
-		if (s1[i] < s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		else if (s1[i] == s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		else
+		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 	}
 	return (0);
