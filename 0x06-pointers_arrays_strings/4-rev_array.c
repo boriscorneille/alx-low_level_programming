3dd37423d08f1e67efd69456eca5769408a0f787
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-06-29 07:01:48 -0700

    4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - will print elements of an array in reverse
+ * @n: will represent elements of an array
+ * @a: a pointer to the array to be reversed
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int c;
+
+	for (i = 0 ; i < n/2 ; i++)
+	{
+		c = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = c;
+	}
+}
