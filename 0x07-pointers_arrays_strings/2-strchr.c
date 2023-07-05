b7407159b871d63e701a9672632b4a7323a55ef1
boriscorneille <132352964+boriscorneille@users.noreply.github.com>
2023-07-05 08:42:45 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+
+/**
+ * *_strchr - locates a character in a string
+ * @c: character to be located
+ * @s: string containing character
+ * Return: return string s if c is fund , else return NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *a = NULL;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (a);
+}
